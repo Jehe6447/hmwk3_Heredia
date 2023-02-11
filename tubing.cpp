c4f8d9bbf3bf1c198961ef32a5deba2bb58d681f
Jehe6447 <114559763+Jehe6447@users.noreply.github.com>
2023-02-11 11:04:34 -0700

    Create tubing.cpp

---
+++ tubing.cpp
@@ -0,0 +1,38 @@
+// CSCI 1300 Spring 2023
+//Author: Jeremiah Heredia
+//Recitation: 102- Tuan Tran
+// Homework 3 Problem 1
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    // declare all the variables
+    int flowrate;
+
+    // prompt the user & get their input
+    cout << "What is the flowrate of Boulder Creek?" << endl;
+    cin >> flowrate;
+
+    // input validation: flowrate must be positive
+    if (flowrate<= 0) 
+    {
+        cout << "Please enter a valid input." << endl;
+        return 0;
+    }
+
+    // decide if it's safe based on flowrate
+    if (flowrate <= 250  ) 
+    {
+        cout << "It is safe to go tubing. Have fun!" << endl;
+    }
+    else
+    {
+        cout << "The river is flowing too fast to go tubing." << endl;
+    }
+
+    return 0;
+}
